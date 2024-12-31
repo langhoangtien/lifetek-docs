c2eb9fec7988ea18622a7c01825da6986e11f58e
Hoang Tien Lang <langhoangtien@gmail.com>
2024-12-31 14:02:55 +0700

    update database oracle

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,6 +1,20 @@
+import { arch } from "os";
+
 export default {
   index: "Introduction",
+  development: "Setup & Development",
+  technologies: "Technologies",
+  architecture: "Architecture",
+
   "api-d": "API",
+  database: "Database",
+  security: "Security",
+  testing: "Testing",
+  benchmarking: "Benchmarking",
+  deployment: "Deployment",
+  troubleshooting: "Troubleshooting",
+  faq: "FAQ",
+
   conventions: "Conventions",
 
   about: {
