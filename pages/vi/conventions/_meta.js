d81325f6e2319271d9266f0c835d3b827bf1cb34
Hoang Tien Lang <langhoangtien@gmail.com>
2024-12-31 21:02:26 +0700

    fix multiple language 2

--- pages/vi/conventions/_meta.js
+++ pages/vi/conventions/_meta.js
@@ -1,8 +1,8 @@
 export default {
   "branch-conventions": "Quy ước nhánh",
-  "clean-code-typescript": "Mã sạch với TypeScript",
+  "clean-code-typescript": "Clean code với TypeScript",
   "commit-conventions": "Quy ước Commit",
-  "linting-and-formatting": "Kiểm tra và định dạng mã",
-  "naming-cheatsheet": "Ghi chú đặt tên",
-  styleguide: "Hướng dẫn phong cách",
+  "linting-and-formatting": "Linting & Formatting",
+  "naming-cheatsheet": "Quy ước đặt tên",
+  styleguide: "Styleguide",
 };
