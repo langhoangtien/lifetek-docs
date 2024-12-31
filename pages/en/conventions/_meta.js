6912cf39711b91d06015cb8b6e100dc0a873c1d3
Hoang Tien Lang <langhoangtien@gmail.com>
2024-12-31 20:35:27 +0700

    fix multiple language

---
+++ pages/en/conventions/_meta.js
@@ -0,0 +1,8 @@
+export default {
+  "branch-conventions": "Branch Conventions",
+  "clean-code-typescript": "Clean Code with TypeScript",
+  "commit-conventions": "Commit Conventions",
+  "linting-and-formatting": "Linting and Formatting",
+  "naming-cheatsheet": "Naming Cheatsheet",
+  styleguide: "Style Guide",
+};
